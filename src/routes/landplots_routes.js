71db5edbc5b17ab229b33487e85457cda4accf13
Omar Rumiche <rumicheomar@gmail.com>
2024-07-15 16:29:28 -0500

    deploy solved

--- src/routes/landplots_routes.js
+++ src/routes/landplots_routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {PrismaClient} from '@prisma/client'
+import {PrismaClient} from '../generated/client/index.js'
 
 const router = Router();
 const prisma = new PrismaClient();
